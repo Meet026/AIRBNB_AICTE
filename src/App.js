b30e82fd2e088735a20469c02b27e04bb98b9e06
Meet <meetpokal04@gmail.com>
2024-12-21 22:10:16 +0530

    Complete Frontend

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme } from "./utils/Themes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./componnents/Navbar";
+import { useState } from "react";
+import Home from "./pages/Home";
+import Authentication from "./pages/Authentication";
+import { useSelector } from "react-redux";
+import ToastMessage from "./componnents/ToastMessage";
+import PropertyDetails from "./pages/PropertyDetails";
+import PropertyListing from "./pages/PropertyListing";
+import background from "./utils/Images/Background.jpg";
+import ContactForm from "./componnents/ContactForm";
+import GalleryPage from "./componnents/GalleryPage";
+import GalleryDetails from "./pages/GalleryDetailes";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+  background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.5)),
+    url(${({ background }) => background});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+function App() {
+  const [openAuth, setOpenAuth] = useState(false);
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container background={background}>
+          <Navbar setOpenAuth={setOpenAuth} openAuth={openAuth} />
+          <Routes>
+            <Route path="/" exact element={<Home />}></Route>
+            <Route path="/contact" exact element={<ContactForm />}></Route>
+            <Route path="/gallery" exact element={<GalleryPage />}></Route>
+            <Route path="/gallery/:id" exact element={<GalleryDetails />}></Route>
+            <Route
+              path="/properties"
+              exact
+              element={<PropertyListing />}
+            ></Route>
+            <Route
+              path="/properties/:id"
+              exact
+              element={<PropertyDetails />}
+            ></Route>
+          </Routes>
+          {openAuth && (
+            <Authentication setOpenAuth={setOpenAuth} openAuth={openAuth} />
+          )}
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/PropertyListing.jsx
@@ -0,0 +1,245 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import PropertyCard from "../componnents/Cards/PropertyCard";
+import { getAllCourses, getAllProperty } from "../api";
+import { CircularProgress } from "@mui/material";
+import { useLocation } from "react-router-dom";
+
+const Container = styled.div`
+  padding: 20px 0;
+  height: 95vh;
+  overflow-y: hidden;
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  @media (max-width: 768px) {
+    padding: 20px 12px;
+    flex-direction: column;
+    overflow-y: scroll;
+  }
+  background: ${({ theme }) => theme.bg};
+  margin: 0 20px;
+  border-radius: 12px 12px 0 0;
+`;
+const Property = styled.div`
+  padding: 12px;
+  overflow: hidden;
+  height: fit-content;
+  @media (min-width: 768px) {
+    width: 100%;
+    overflow-y: scroll;
+    height: 100%;
+  }
+`;
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px;
+  justify-content: center;
+  @media (max-width: 750px) {
+    gap: 14px;
+  }
+`;
+
+const PropertyListing = () => {
+  const [properties, setProperties] = useState([
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29848858/pexels-photo-29848858/free-photo-of-scenic-twilight-view-of-lakefront-in-lombardy.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Beautiful Beach House",
+      desc: "A stunning house located near the beach with amazing sea views.",
+      rating: "4.5",
+      location: "Los, Angles",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29851526/pexels-photo-29851526/free-photo-of-charming-waterfront-in-saint-tropez-france.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Royal Heritage Haven",
+      desc: "A grand palace with intricate carvings, vibrant frescoes, and rich cultural history dating back centuries.",
+      rating: "4",
+      location: "Paris, France",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29843678/pexels-photo-29843678/free-photo-of-ancient-rock-tomb-in-scenic-mugla-landscape.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Timeless Cityscape",
+      desc: "A cobblestone street surrounded by ancient buildings, quaint cafes, and iconic landmarks.",
+      rating: "3.5",
+      location: "Rome, Italy",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29821515/pexels-photo-29821515/free-photo-of-cloud-gate-sculpture-in-millennium-park-chicago.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Majestic Palace Retreat",
+      desc: "A historic palace with grand courtyards, ornate interiors, and centuries-old stories",
+      rating: "3",
+      location: "London, UK",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29828027/pexels-photo-29828027/free-photo-of-scenic-winter-view-of-tatra-mountains-in-zakopane.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Frosted Pines Chalet",
+      desc: "A cozy chalet amidst snowy landscapes with fireplaces and hot cocoa waiting for you.",
+      rating: "4",
+      location: "Zurich, Switzerland",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29787589/pexels-photo-29787589/free-photo-of-stunning-star-trails-over-cao-b-ng-mountains.png?auto=compress&cs=tinysrgb&w=600",
+      title: "Rocky Ridge Adventure",
+      desc: "Rugged mountains with steep trails, perfect for climbing and adventure seekers.",
+      rating: "4.5",
+      location: "Sydney, Australia",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29821515/pexels-photo-29821515/free-photo-of-cloud-gate-sculpture-in-millennium-park-chicago.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Majestic Palace Retreat",
+      desc: "A historic palace with grand courtyards, ornate interiors, and centuries-old stories",
+      rating: "3",
+      location: "London, UK",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29828027/pexels-photo-29828027/free-photo-of-scenic-winter-view-of-tatra-mountains-in-zakopane.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Frosted Pines Chalet",
+      desc: "A cozy chalet amidst snowy landscapes with fireplaces and hot cocoa waiting for you.",
+      rating: "4",
+      location: "Zurich, Switzerland",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29787589/pexels-photo-29787589/free-photo-of-stunning-star-trails-over-cao-b-ng-mountains.png?auto=compress&cs=tinysrgb&w=600",
+      title: "Rocky Ridge Adventure",
+      desc: "Rugged mountains with steep trails, perfect for climbing and adventure seekers.",
+      rating: "4.5",
+      location: "Sydney, Australia",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29848858/pexels-photo-29848858/free-photo-of-scenic-twilight-view-of-lakefront-in-lombardy.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Beautiful Beach House",
+      desc: "A stunning house located near the beach with amazing sea views.",
+      rating: "4.5",
+      location: "Los, Angles",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29851526/pexels-photo-29851526/free-photo-of-charming-waterfront-in-saint-tropez-france.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Royal Heritage Haven",
+      desc: "A grand palace with intricate carvings, vibrant frescoes, and rich cultural history dating back centuries.",
+      rating: "4",
+      location: "Paris, France",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29843678/pexels-photo-29843678/free-photo-of-ancient-rock-tomb-in-scenic-mugla-landscape.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Timeless Cityscape",
+      desc: "A cobblestone street surrounded by ancient buildings, quaint cafes, and iconic landmarks.",
+      rating: "3.5",
+      location: "Rome, Italy",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29821515/pexels-photo-29821515/free-photo-of-cloud-gate-sculpture-in-millennium-park-chicago.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Majestic Palace Retreat",
+      desc: "A historic palace with grand courtyards, ornate interiors, and centuries-old stories",
+      rating: "3",
+      location: "London, UK",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+    {
+      _id: "12345",
+      img: "https://images.pexels.com/photos/29828027/pexels-photo-29828027/free-photo-of-scenic-winter-view-of-tatra-mountains-in-zakopane.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Frosted Pines Chalet",
+      desc: "A cozy chalet amidst snowy landscapes with fireplaces and hot cocoa waiting for you.",
+      rating: "4",
+      location: "Zurich, Switzerland",
+      price: {
+        org: 250,
+        mrp: 300,
+        off: 17,
+      },
+    },
+  ]);
+
+  return (
+    <Container>
+      <Property>
+        <CardWrapper>
+          {
+            properties.map((property) => (
+              <PropertyCard property={property}/>
+            ))
+          }
+        </CardWrapper>
+      </Property>
+    </Container>
+  )
+};
+
+export default PropertyListing;
